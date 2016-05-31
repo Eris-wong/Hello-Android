fbed9864bc605b97105a1ab186ceb378f2c13510
yhwang <yhwang@estt.com.cn>
2016-05-31 16:31:09 +0800

    a

---
+++ src/com/work1/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.work1;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out print("Hello");
+
+	}
+
+}
